1e44cdbb1eda735f0ba80fc51660f57ca9f637ee
artyom <awave103fm@mail.ru>
2024-05-23 21:19:59 +0300

    start create book page

--- routes/routes.js
+++ routes/routes.js
@@ -6,9 +6,12 @@ router.get('/',getIndex)
 //Получение жанров и поджанров
 router.get('/json/get-genre',getGenre)
 router.get('/json/get-subgenre',getSubgenre)
-//Получение книг
+//Получение книг этого года(новинок)
 router.get('/json/get-new-book-row', getNewBookRow)
 //Маршрут для картинок
-router.get('/images/:imageName',getImage)
-
+router.get('/get-image/:imageName',getImage)
+//Маршрут для страницы книги
+router.get('/content/get-book/:bookName')
+//Маршрут для содуржимого страницы книги
+router.get('/json/get-book/:bookName')
 export const exportRouter = router
